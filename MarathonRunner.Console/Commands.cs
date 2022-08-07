64f524fc4a3688a19bf366c235b691df81203254
terry_u16 <terry-u16@users.noreply.github.com>
2022-08-07 22:10:27 +0900

    ランメソッドを作成

--- MarathonRunner.Console/Commands.cs
+++ MarathonRunner.Console/Commands.cs
@@ -1,6 +1,21 @@
-﻿namespace TerryU16.MarathonRunner.Console;
+﻿using TerryU16.MarathonRunner.Core.Runners;
+
+namespace TerryU16.MarathonRunner.Console;
 
 public class Commands : ConsoleAppBase
 {
-    
+    private readonly LocalRunner _localRunner;
+    private readonly CloudRunner _cloudRunner;
+
+    public Commands(LocalRunner localRunner, CloudRunner cloudRunner)
+    {
+        _localRunner = localRunner;
+        _cloudRunner = cloudRunner;
+    }
+
+    [Command("run-local", "ローカルでテストケースを実行します。")]
+    public async Task RunLocalAsync() => await _localRunner.RunAsync(Context.CancellationToken);
+
+    [Command("run-cloud", "クラウド上でテストケースを実行します。")]
+    public async Task RunCloudAsync() => await _cloudRunner.RunAsync(Context.CancellationToken);
 }
